24a03e4d597c684e393aabaefb62f795a2f561eb
Pedro Duarte <phduarte2704@gmail.com>
2024-03-31 18:59:56 -0300

    segundo commit do js

--- script.js
+++ script.js
@@ -8,3 +8,30 @@ function receberValorConta(evento) {
 
     console.log(conta)
 }
+
+const pessoasInput = document.querySelector("#pessoas")
+pessoasInput.addEventListener("input", receberQunatidaePessoas)
+
+function receberQunatidaePessoas(evento){
+    const paragrafoErro = document.querySelector(".pessoas #erro")
+    const divErro = document.querySelector(".pessoas .input-box")
+    
+    
+    
+    if(evento.target.value === "0"){
+        paragrafoErro.computedStyleMap.display = "block"
+        divErro.setAttribute("id", "erro-div")
+    }else {
+        paragrafoErro.computedStyleMap.display = "none"
+        divErro.setAttribute("id", "")
+        pessoas = Number(evento.target.value)
+    }  
+
+}
+
+const botoesGorjeta = document.querySelector(".gorjeta input[type='button']")
+botoesGorjeta.forEach(botao => {
+    botao.addEventListener("click",)
+})
+
+function receberPorcentagem(evento) {
